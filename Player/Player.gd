c02ca3d0a0d1f1f5c3e241e90525f1916156768a
rans0000 <rans0000@gmail.com>
2020-05-10 22:03:46 +0530

    Remove duplicate environment meshes.

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,20 +1,24 @@
 extends KinematicBody
 
-var gravity = -9.8
+var gravity = -65
 var velocity = Vector3()
+var previous_speed = Vector3()
 var strafe = {'left': Vector3(-1,0,0), 'idle': Vector3(), 'right': Vector3(1,0,0)}
 var strafe_direction = strafe.idle
 
 const STRAFE_BOUNDARY = 10
-const F_SPEED = 40
+const F_SPEED = 50
 const S_SPEED = 80
-const ACCELERATION = 5
-const DECELERATION = 2
+const ACCELERATION = 1
+const DECELERATION = 3
+const FRICTION = 0.1
 
 var is_jumping = false
 var is_powering_jump = false
 var jump_power = 0
-const VERTICAL_JUMP = 4
+const MIN_JUMP_SPEED = 20
+const MAX_JUMP_FORWARD_RATIO = 0.3
+const MAX_JUMP_POWER = 20.0
 
 func _physics_process(delta):
 	move_player(delta)
@@ -40,12 +44,14 @@ func move_forward(delta):
 		forward_velocity = Vector3.BACK
 	
 	var fv = Vector3(0, 0, velocity.z)
-	var new_pos = forward_velocity * F_SPEED
-	var acceleration = DECELERATION
-	if forward_velocity.dot(fv) > 0:
-		acceleration = ACCELERATION
-	
-	fv = fv.linear_interpolate(new_pos, acceleration * delta)
+	if is_floored:
+		var new_pos = forward_velocity * F_SPEED
+		previous_speed = new_pos
+		var acceleration = DECELERATION
+		if forward_velocity.dot(fv) > 0:
+			acceleration = ACCELERATION
+		
+		fv = fv.linear_interpolate(new_pos, acceleration * delta)
 	return fv
 
 func move_sideways():
@@ -68,21 +74,31 @@ func jump():
 	var is_floored = is_on_floor()
 	if is_jumping and is_floored:
 		is_jumping = false
+		jump_power = 0
 	
 	if not is_floored:
 		is_powering_jump = false
 	
 	if is_powering_jump:
-		++jump_power
+		jump_power += 1
+		jump_power = clamp(jump_power, 0, MAX_JUMP_POWER)
+		#print('jump_power: ', jump_power)
 	
 	if not is_jumping and Input.is_action_just_pressed("jump"):
 		is_powering_jump = true
 	if not is_jumping and Input.is_action_just_released("jump"):
 		is_powering_jump = false
 		is_jumping = true
-		#80% forward speed is added to jump speed
-		jump_speed.y += (VERTICAL_JUMP + (-velocity.z * 0.5) )
+		jump_speed.y += calculate_jump_power(jump_speed)
 	return jump_speed
+
+func calculate_jump_power(jump_speed):
+	var vertical_speed = jump_speed.y
+	var speed_ratio = (jump_power/MAX_JUMP_POWER * MAX_JUMP_FORWARD_RATIO) * -velocity.z
+	var power = (MIN_JUMP_SPEED + speed_ratio )
+	vertical_speed += power
+	print(vertical_speed, ' : ', jump_power)
+	return vertical_speed
 
 func clamp_vector(vector, length):
 	var norm = vector.normalized()
