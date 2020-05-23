8d35bccb112dca05dc969e0931441b9ed515f0c0
rans0000 <rans0000@gmail.com>
2020-05-23 17:01:01 +0530

    Cleanup the code

--- Player/Camera.gd
+++ Player/Camera.gd
@@ -1,7 +1,7 @@
 extends Camera
 
 func _input(event):
-	if event.is_action_pressed("ui_down"):
+	if event.is_action_pressed("look_back"):
 		rotate_y(deg2rad(180))
-	elif event.is_action_released("ui_down"):
+	elif event.is_action_released("look_back"):
 		rotate_y(deg2rad(180))
