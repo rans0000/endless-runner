660621789a5d85c1051550e6d10d4998c633b2a5
rans0000 <rans0000@gmail.com>
2020-05-23 20:54:08 +0530

    Add options screen for Controls, Help and About in main menu

--- UI/MainMenu/MainMenu.gd
+++ UI/MainMenu/MainMenu.gd
@@ -10,3 +10,7 @@ func _on_play_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_options_button_pressed():
+	$OptionsMenu.visible = true
---
+++ UI/OptionsMenu/OptionsMenu.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _ready():
+	pass
+
+
+func _on_close_btn_pressed():
+	visible = false
