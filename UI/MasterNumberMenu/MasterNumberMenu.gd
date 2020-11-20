0aafdbe28b4ed3e233fc63cad236adb1bc6e32ad
rans0000 <rans0000@gmail.com>
2020-11-21 02:03:16 +0530

    add theme style to master value selection UI

--- UI/MasterNumberMenu/MasterNumberMenu.gd
+++ UI/MasterNumberMenu/MasterNumberMenu.gd
@@ -1,24 +1,18 @@
 extends Control
 
 onready var Utils = get_node("/root/Utils")
-onready var value_label = $MasterValueLabel;
-onready var slider = $HSlider;
 
 func _ready():
-	slider.value = Utils.puzzle_number
-	value_label.text = str(slider.value)
+	pass
 
 
-func _on_master_value_slider_changed(value):
-	value_label.text = str(value)
-
-
-func _on_select_btn_up():
-	Utils.puzzle_number = int(slider.value)
+func _on_cancel_btn_up():
 	self.visible = false
 	get_tree().paused = false
 
 
-func _on_cancel_btn_up():
+func _on_master_value_click(selected_value):
+	print(selected_value)
+	Utils.puzzle_number = selected_value
 	self.visible = false
 	get_tree().paused = false
